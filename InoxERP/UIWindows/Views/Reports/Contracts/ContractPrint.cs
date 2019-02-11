f280aebe492423e6f4db41e70bc8f9704911a945
RodrigoDonha <rodrigodanieldonha@hotmail.com>
2019-02-11 10:03:14 -0200

    corrections in contract printing

--- InoxERP/UIWindows/Views/Reports/Contracts/ContractPrint.cs
+++ InoxERP/UIWindows/Views/Reports/Contracts/ContractPrint.cs
@@ -54,18 +54,7 @@ namespace UIWindows.Views.Reports.Contracts
                                       ", Bairro: " + searchContracts.sProviderDistrict + 
                                       ", Cidade: " + searchContracts.sProviderCity + 
                                       "/" + searchContracts.ProviderEstate;
-
-            string EstadoContratado = searchContracts.ProviderEstate.ToString();
-
-            string rodapeContratado = searchContracts.sProviderName + "\r\n CNPJ: " +
-                                      searchContracts.sProviderCpfCnpj +
-                                      " RG/Inscr. Est. " + searchContracts.sRegistStateRg + "\r\n " +
-                                      searchContracts.sProviderAdress + ", " +
-                                      searchContracts.sProviderNumberAdress + ", " +
-                                      searchContracts.sProviderDistrict + ", " + searchContracts.sProviderCity + "/" +
-                                      searchContracts.ProviderEstate +
-                                      " - CEP " + searchContracts.sProviderCep;
-                
+            string cnpjProvider = searchContracts.sProviderCpfCnpj;
 
             string objetoString = searchContracts.sClientObjectContract;
             string valorString = searchContracts.dTotalValue.ToString();
@@ -76,7 +65,7 @@ namespace UIWindows.Views.Reports.Contracts
             string cidadeString = searchContracts.sCity;
             string dataString = searchContracts.dtContractDate.ToLongDateString();
             string proprietarioString = searchContracts.sProviderName;
-
+            
             var contratante = new ReportParameter();
             var contratado = new ReportParameter();
             var objeto = new ReportParameter();
@@ -88,9 +77,8 @@ namespace UIWindows.Views.Reports.Contracts
             var cidade = new ReportParameter();
             var data = new ReportParameter();
             var proprietario = new ReportParameter();
-            var rodapeContrato = new ReportParameter();
-            var estadoContratado = new ReportParameter();
-
+            var cnpjProviderRodape = new ReportParameter();
+            
             contratante.Name = "contratante";
             contratado.Name = "contratado";
             objeto.Name = "objeto";
@@ -102,9 +90,8 @@ namespace UIWindows.Views.Reports.Contracts
             cidade.Name = "cidade";
             data.Name = "data";
             proprietario.Name = "proprietario";
-            rodapeContrato.Name = "rodapeContratado";
-            estadoContratado.Name = "estadoContratado";
-
+            cnpjProviderRodape.Name = "cnpjProvider";
+            
             contratante.Values.Add(contratanteString);
             contratado.Values.Add(contratadoString);
             objeto.Values.Add(objetoString);
@@ -116,9 +103,8 @@ namespace UIWindows.Views.Reports.Contracts
             cidade.Values.Add(cidadeString);
             data.Values.Add(dataString);
             proprietario.Values.Add(proprietarioString);
-            rodapeContrato.Values.Add(rodapeContratado);
-            estadoContratado.Values.Add(EstadoContratado);
-
+            cnpjProviderRodape.Values.Add(cnpjProvider);
+            
             reportViewer1.LocalReport.SetParameters(contratante);
             reportViewer1.LocalReport.SetParameters(contratado);
             reportViewer1.LocalReport.SetParameters(objeto);
@@ -130,8 +116,8 @@ namespace UIWindows.Views.Reports.Contracts
             reportViewer1.LocalReport.SetParameters(cidade);
             reportViewer1.LocalReport.SetParameters(data);
             reportViewer1.LocalReport.SetParameters(proprietario);
-            reportViewer1.LocalReport.SetParameters(estadoContratado);
-            reportViewer1.LocalReport.SetParameters(rodapeContrato);
+            reportViewer1.LocalReport.SetParameters(cnpjProviderRodape);
+
 
             reportViewer1.RefreshReport();
         }
